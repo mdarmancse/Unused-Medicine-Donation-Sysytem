be9e8afefd68975d6d46b0b9b06435f0d2f36e11
Md Arman Ullah <65160577+mdarmancse@users.noreply.github.com>
2020-07-14 21:20:20 +0600

    s

--- database/umds.sql
+++ database/umds.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2019 at 11:01 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- Generation Time: Jul 14, 2020 at 05:19 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `umds`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation_form`
+--
+
+CREATE TABLE `donation_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `division` varchar(1000) NOT NULL,
+  `postal_code` varchar(1000) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `medicines_name` varchar(1000) NOT NULL,
+  `exp_date` varchar(1000) NOT NULL,
+  `amount` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `send_mail`
+--
+
+CREATE TABLE `send_mail` (
+  `id` int(255) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(1000) NOT NULL,
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -41,8 +72,27 @@ CREATE TABLE `tbl_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `name`, `email_address`, `password`, `phone_number`, `district`, `dob`, `gender`, `profile_picture`) VALUES
+(47, 'Md Arman', 'amd55077@gmail.com', '123', '01787281564', 'Ctg', '', 'Male', '');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `donation_form`
+--
+ALTER TABLE `donation_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `send_mail`
+--
+ALTER TABLE `send_mail`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_users`
@@ -55,10 +105,22 @@ ALTER TABLE `tbl_users`
 --
 
 --
+-- AUTO_INCREMENT for table `donation_form`
+--
+ALTER TABLE `donation_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `send_mail`
+--
+ALTER TABLE `send_mail`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
